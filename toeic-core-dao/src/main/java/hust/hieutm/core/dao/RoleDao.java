445155079c6527580c72a8f5891030ef4b504de6
HieuTM <minhhieu.tran0407@gmail.com>
2019-07-02 11:07:01 +0700

    init project

---
+++ toeic-core-dao/src/main/java/hust/hieutm/core/dao/RoleDao.java
@@ -0,0 +1,7 @@
+package hust.hieutm.core.dao;
+
+import hust.hieutm.core.data.dao.GenericDao;
+import hust.hieutm.core.persistence.entity.RoleEntity;
+
+public interface RoleDao extends GenericDao<Integer, RoleEntity> {
+}
