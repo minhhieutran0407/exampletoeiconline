2bee4e0c762e108de1d893b77692703f59d0918b
HieuTM <minhhieu.tran0407@gmail.com>
2019-07-02 16:59:17 +0700

    delete all

--- toeic-core-dao-impl/src/main/java/hust/hieutm/core/daoimpl/RoleDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package hust.hieutm.core.daoimpl;
-
-import hust.hieutm.core.dao.RoleDao;
-import hust.hieutm.core.data.daoimpl.AbstractDao;
-import hust.hieutm.core.persistence.entity.RoleEntity;
-
-public class RoleDaoImpl extends AbstractDao<Integer, RoleEntity> implements RoleDao {
-}
--- toeic-core-dao/src/main/java/hust/hieutm/core/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package hust.hieutm.core.dao;
-
-import hust.hieutm.core.data.dao.GenericDao;
-import hust.hieutm.core.persistence.entity.RoleEntity;
-
-public interface RoleDao extends GenericDao<Integer, RoleEntity> {
-}
